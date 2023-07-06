8abe0e8c9aee7600ab18d6376a766ef03a288d9d
Sarah Wesley <srwesley722@gmail.com>
2023-07-05 23:57:19 -0500

    Mostly completed js file

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChar = "0123456789";
-var specialChar = "!#$%&()/*+-";
- 
 function generatePassword() {
   console.log("The button was clicked!");
+  
+  var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialChar = ['!', '#', '$', '%', '&'];
+  var randomCharacters = [];
 
   numberOfCharacters = prompt("Choose a number of characters wanted in your password, which is between 8 and 128 characters.");
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
@@ -20,7 +21,7 @@ function generatePassword() {
 
   lowercaseCharacters = confirm("Do you want to use lowercase characters?");
   if (lowercaseCharacters) {
-    var toLowerCase = alert("You have selected to use lowercase characters.");
+    alert("You have selected to use lowercase characters.");
   } else {
     alert("You have opted out of using lowercase characters.");
   }
@@ -45,8 +46,28 @@ function generatePassword() {
   } else {
     alert("You have opted out of using special characters.");
   }
-  // return "Random password goes here!"
-}
+
+  if (lowercaseCharacters === false && upperCaseCharacters === false && numbers === false && specialCharacters === false) {
+    return "Please use at least one character type, thanks.";
+  };
+
+  if (lowercaseCharacters) {
+    randomCharacters = randomCharacters.concat(lowerCaseChar);
+  } if (upperCaseCharacters) {
+    randomCharacters = randomCharacters.concat(upperCaseChar);
+  } if (numbers) {
+    randomCharacters = randomCharacters.concat(numbericChar);
+  } if (specialCharacters) {
+    randomCharacters = randomCharacters.concat(specialChar);
+  };
+
+  let randomPassword = ""
+  for (let i = 0; i < numberOfCharacters; i++) {
+    let randomized = [Math.floor(Math.random() * randomCharacters.length)];
+    randomPassword = randomPassword + randomCharacters[randomized];
+  }
+  return randomPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
